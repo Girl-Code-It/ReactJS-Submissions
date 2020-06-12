00aaabd1a1cc91156a75d6d266a0af729a0dec11
Meghag5 <meghagupta05012001@gmail.com>
2020-06-12 16:05:39 +0530

    Milestone 1

---
+++ megha/milestone 1/index.js
@@ -0,0 +1,8 @@
+import React from "react"
+import ReactDOM from "react-dom"
+
+//JSX
+ReactDOM.render(<div><h1>Hello world!</h1><p>This is a paragraph</p></div>,document.getElementById("root"))
+
+var myNewP = document.createElement("p")
+myNewP.innerHTML = "This is a paragraph."
